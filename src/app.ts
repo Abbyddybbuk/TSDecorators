5d15ba3c4197ec5d457fc71abd0771bfaf0c7e7d
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-06-13 12:19:55 +0100

    More Useful Decorators

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,23 @@ function Logger(logString: string) {
   };
 }
 
+function WithTemplate(template: string, hookId: string) {
+  return function(constructor: any) {
+//   return function (_: Function) {//the _ signals to TS is that yes there is a parameter expected but we do not need it
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
 //Decorator Factory: Advantage of passing values in function and calling function within function is that you can pass values inside your decorators
-@Logger('Logging - Person Name') // This annotation is a decorator which will print the class constructor based on function above
+// @Logger('Logging - Person Name') // This annotation is a decorator which will print the class constructor based on function above
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
-  name = "Abhijeet";
+  name = "Abhijeet Kulshreshtha";
 
   constructor() {
     console.log("Creating Person Object....");
