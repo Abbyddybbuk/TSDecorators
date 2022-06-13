a35c88379d7b975918be2ddf96bdf1f2abae79dd
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-06-13 12:24:48 +0100

    Decorator Execution Sequence

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,28 @@
 function Logger(logString: string) {
+  console.log("Logger Function");
   return function (constructor: Function) {
+    console.log("Logger Decorator");
     console.log(logString);
     console.log(constructor);
   };
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function(constructor: any) {
-//   return function (_: Function) {//the _ signals to TS is that yes there is a parameter expected but we do not need it
+  console.log("WithTemplate Function");
+  return function (constructor: any) {
+    //   return function (_: Function) {//the _ signals to TS is that yes there is a parameter expected but we do not need it
+    console.log("WithTemplate Decorator");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
       hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
   };
 }
 
 //Decorator Factory: Advantage of passing values in function and calling function within function is that you can pass values inside your decorators
-// @Logger('Logging - Person Name') // This annotation is a decorator which will print the class constructor based on function above
+@Logger("Logging - Person Name") // This annotation is a decorator which will print the class constructor based on function above
 @WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Abhijeet Kulshreshtha";
