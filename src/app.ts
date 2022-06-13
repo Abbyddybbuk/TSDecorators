f6b2d036d2422861a3e3273ea803219c32076000
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-06-13 12:05:52 +0100

    Decorator: Logger

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
-function Logger(constructor: Function) {
-    console.log('Logging...');
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
     console.log(constructor);
+  };
 }
 
-@Logger// This annotation is a decorator which will print the class constructor based on function above
+//Decorator Factory: Advantage of passing values in function and calling function within function is that you can pass values inside your decorators
+@Logger('Logging - Person Name') // This annotation is a decorator which will print the class constructor based on function above
 class Person {
-    name = 'Abhijeet';
+  name = "Abhijeet";
 
-    constructor() {
-        console.log('Creating Person Object....')
-    }
+  constructor() {
+    console.log("Creating Person Object....");
+  }
 }
 
-// const pers = new Person(); //Lgger will get printed irrespective whether object has been created or not
-// console.log(pers);
+const pers = new Person(); //Logger will get printed irrespective whether object has been created or not
+console.log(pers);
