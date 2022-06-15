7c0526df55a374cbc81232094a87f491e6e3278c
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-06-15 01:05:53 +0100

    Autobind example with defaulting this keyword to current class

--- src/app.ts
+++ src/app.ts
@@ -96,3 +96,30 @@ class Product {
     return this._price * (1 + tax);
   }
 }
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;//descriptor.value gives existing method name
+  const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: false,
+      get() {
+          const boundFn = originalMethod.bind(this);
+          return boundFn;
+      }
+  };
+  return adjDescriptor;
+}
+
+
+class Printer {
+    message = 'Awesome Decorators!';
+    
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
